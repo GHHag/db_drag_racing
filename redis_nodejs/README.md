82ab294aff02d99f0342ffa628bd10b6eb1bafd6
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-03-20 10:40:01 +0100

    added README.md file with some info

---
+++ redis_nodejs/README.md
@@ -0,0 +1,4 @@
+### DOCUMENTATION:
+
+1. cd into redis_nodejs and write: npm install
+2. to start the API endpoints: cd int src and write: npm run dev
--- redis_nodejs/src/index.js
+++ redis_nodejs/src/index.js
@@ -5,8 +5,12 @@ const client = redis.createClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Now we're able to receive the JSON data inside our controllers under req.body.
-app.use(bodyParser.json());
-// app.use('/api/v1/workouts', v1WorkoutRouter);
+// app.use(bodyParser.json());
+
+// For testing purposes
+app.get('/', (req, res) => {
+  res.send("<h2>It's Working!</h2>");
+});
 
 app.listen(PORT, () => {
   console.log(`API listening on port ${PORT}`);
