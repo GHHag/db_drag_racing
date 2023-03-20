28ebd8a432b1db305e3d39350a2b6fe83002cf5b
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-03-20 10:17:19 +0100

    added redis_nodejs folder

---
+++ redis_nodejs/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const redis = require('redis');
+const client = redis.createClient();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Now we're able to receive the JSON data inside our controllers under req.body.
+app.use(bodyParser.json());
+// app.use('/api/v1/workouts', v1WorkoutRouter);
+
+app.listen(PORT, () => {
+  console.log(`API listening on port ${PORT}`);
+});
