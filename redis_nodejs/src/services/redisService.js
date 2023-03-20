2d77b069d9a654478ee4d88c9df3dc587984c50c
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-03-20 12:31:19 +0100

    under the hood controller talks with Service

---
+++ redis_nodejs/src/services/redisService.js
@@ -0,0 +1,22 @@
+const getAllKeys = () => {
+  return;
+};
+
+const getAKey = () => {
+  return;
+};
+
+const postAKey = () => {
+  return;
+};
+
+const deleteAKey = () => {
+  return;
+};
+
+module.exports = {
+  getAllKeys,
+  getAKey,
+  postAKey,
+  deleteAKey,
+};
