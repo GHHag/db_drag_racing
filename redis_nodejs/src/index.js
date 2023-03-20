cf976666dc8bd1c60eb6b23d1b3cb6e6c73d9dce
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-03-20 15:38:16 +0100

    added the API into index.js, have not removed the controllers, services or routes

--- redis_nodejs/src/index.js
+++ redis_nodejs/src/index.js
@@ -1,16 +1,66 @@
 const express = require('express');
-const v1Router = require('./v1/routes/redisRoutes');
 const redis = require('redis');
 const client = redis.createClient();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-// Now we're able to receive the JSON data inside our controllers under req.body.
-// app.use(bodyParser.json());
+const PORT = 3000;
 
-// V1 API ROUTES
-app.use('/api/v1/redis', v1Router);
+// Parse incoming JSON data
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`API listening on port ${PORT}`);
+// GET all keys
+app.get('/keys', (req, res) => {
+  client.keys('*', (err, keys) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving keys from Redis');
+    } else {
+      res.send(keys);
+    }
+  });
 });
+
+// GET a value by key
+app.get('/get/:key', (req, res) => {
+  const key = req.params.key;
+  client.get(key, (err, value) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(`Error retrieving value for key ${key}`);
+    } else if (value === null) {
+      res.status(404).send(`No value found for key ${key}`);
+    } else {
+      res.send(value);
+    }
+  });
+});
+
+// POST a value with a key
+app.post('/set', (req, res) => {
+  const { key, value } = req.body;
+  client.set(key, value, (err, reply) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(`Error setting value for key ${key}`);
+    } else {
+      res.send(`Value set for key ${key}`);
+    }
+  });
+});
+
+// DELETE a value by key
+app.delete('/delete/:key', (req, res) => {
+  const key = req.params.key;
+  client.del(key, (err, reply) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(`Error deleting value for key ${key}`);
+    } else if (reply === 0) {
+      res.status(404).send(`No value found for key ${key}`);
+    } else {
+      res.send(`Value deleted for key ${key}`);
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- redis_nodejs/src/services/redisService.js
+++ redis_nodejs/src/services/redisService.js
@@ -1,17 +1,54 @@
+const redis = require('redis');
+const client = redis.createClient();
+
 const getAllKeys = () => {
-  return;
+  client.keys('*', (err, keys) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving keys from Redis');
+    } else {
+      res.send(keys);
+    }
+  });
 };
 
 const getAKey = () => {
-  return;
+  const key = req.params.key;
+  client.get(key, (err, value) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(`Error retrieving value for key ${key}`);
+    } else if (value === null) {
+      res.status(404).send(`No value found for key ${key}`);
+    } else {
+      res.send(value);
+    }
+  });
 };
 
 const postAKey = () => {
-  return;
+  const { key, value } = req.body;
+  client.set(key, value, (err, reply) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(`Error setting value for key ${key}`);
+    } else {
+      res.send(`Value set for key ${key}`);
+    }
+  });
 };
 
 const deleteAKey = () => {
-  return;
+  client.del(key, (err, reply) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(`Error deleting value for key ${key}`);
+    } else if (reply === 0) {
+      res.status(404).send(`No value found for key ${key}`);
+    } else {
+      res.send(`Value deleted for key ${key}`);
+    }
+  });
 };
 
 module.exports = {
