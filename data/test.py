54029d6ad9817ae0ed0ccdf081761690baf8c644
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-03-31 12:18:28 +0200

    .

--- data/test.py
+++ data/test.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+# Läser in och formaterar datan som skickas sedan till Redis server
+# Detta kan bli en Locust fil!
 with open('generated_data.json', 'r') as file:
     json_data = json.loads(file.read())
     for i in json_data:
@@ -11,5 +12,5 @@ with open('generated_data.json', 'r') as file:
         data = {k: v for k, v in i.items() if k not in ['retailUnit', 'id']}
         data_str = json.dumps(data)
         requests.post(
-            f"localhost:8080/redis/hset?hash={retail_unit}&key={id}&value={data_str}"
+            f"http://localhost:8080/redis/hset?hash={retail_unit}&key={id}&value={data_str}"
         )
