e05b089b718fd770a10336db5762e421bfd7891f
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-04-05 16:17:23 +0200

    Small change in locustfile.py

--- locust-service/locustfile.py
+++ locust-service/locustfile.py
@@ -2,13 +2,11 @@ from locust import HttpUser, task
 
 
 class MyUser(HttpUser):
-    @task(1)
+    # host: "/db-api/src/app.py"
+
+    @task
     def create_post(self):
         self.client.post(
-            "/redis/hset?hash=UA&key=52fe86a8-d222-11ed-90cb-5254004a31de",
+            "/redis/hset?hash=PT&key=a0014cde-ea93-4d95-8260-273198c50a95",
             headers={"content-type": "application/json"},
         )
-
-
-class WebsiteUser(HttpUser):
-    task_set = MyUser
