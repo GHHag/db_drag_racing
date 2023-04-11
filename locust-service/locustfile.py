afe78a88cd5c827b8a7cdddc8a8bcf7ec86a3a32
GHHag <gustav.hagenblad@gmail.com>
2023-04-11 16:20:13 +0200

    Dockerfiles

--- locust-service/locustfile.py
+++ locust-service/locustfile.py
@@ -11,10 +11,9 @@ class MyUser(HttpUser):
         # Call constructor of super class (HTTPUser)
         super(MyUser, self).__init__(*args, **kwargs)
         
-        # Data is always data
         self.data_key = 'data'
         
-        # Define hash and unique key for CAR
+        # Define hash and unique key for cars
         self.car_hash_key = 'id'
         self.car_unique_key = 'id'
         
@@ -23,42 +22,52 @@ class MyUser(HttpUser):
         self.reparation_hash_key = 'car_id'
         self.reparation_unique_key = 'id'
 
+        # Define hash and unique key for parts
         self.part_hash_key = 'reparation_id'
         self.part_unique_key = 'id'
 
         # Read in data
         self.cars_data_list = format_json_data("./data/cars_data.json", self.car_hash_key, self.car_unique_key)
         self.reparations_data_list= format_json_data("./data/reparations_data.json", self.reparation_hash_key, self.reparation_unique_key)
-        self.parts_data_list = []
+        self.parts_data_list = format_json_data("./data/parts_data.json", self.part_hash_key, self.part_unique_key)
     
     # A post request to API (create)
     # If len > 0 then pop so we dont write the same line twice
     # First tasks saves cars_data_json to Redis
-    @task(2)
+    @task
     def create_car_post(self):   
         if len(self.cars_data_list):
             data_entry = self.cars_data_list.pop(0)
             self.client.post(
                 f'/redis/hset?hash={data_entry.get(self.car_hash_key)}&key={data_entry.get(self.car_unique_key)}&value={data_entry.get(self.data_key)}',
-                headers={"content-type": "application/json"},
+                headers={"content-type": "application/json"}
             )
         else:
             pass
             #exit(1)
     
-    # Now we are sending reparations for those cars that are already inserted
-    @task(1)
+    @task
     def create_reparations_post(self):
         if len(self.reparations_data_list):
             data_entry = self.reparations_data_list.pop(0)
             self.client.post(
                 f'/redis/hset?hash={data_entry.get(self.reparation_hash_key)}&key={data_entry.get(self.reparation_unique_key)}&value={data_entry.get(self.data_key)}',
-                headers={"content-type": "application/json"},
+                headers={"content-type": "application/json"}
+            )
+
+    @task
+    def create_parts_post(self):
+        if len(self.parts_data_list):
+            data_entry = self.parts_data_list.pop(0)
+            self.client.post(
+                f'/redis/hset?hash={data_entry.get(self.part_hash_key)}&key={data_entry.get(self.part_unique_key)}&value={data_entry.get(self.data_key)}',
+                headers={"content-type": "application/json"}
             )
     
     wait_time = between(0,1)
 
-# When the script is running (python locust.py), this if statement will be executed
+
+# When running this script (python locust.py), this if statement will be executed
 if __name__ == "__main__":
     cars_data_list = format_json_data("./data/cars_data.json", "retailUnit", "id")
     reparations_data_list = format_json_data("./data/reparations_data.json", "car_id", "id")
@@ -70,5 +79,3 @@ if __name__ == "__main__":
     # print()
     # pprint.pprint(parts_data_list[0])
     # print()
-
-    
