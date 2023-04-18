ea5bce15e996dc6a3d70efee4242dbb6f26e16eb
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-04-18 15:47:27 +0200

    Added some gcp logic, but needs rework

--- db-api/src/clients/bigtable_client.py
+++ db-api/src/clients/bigtable_client.py
@@ -1,4 +1,7 @@
+import datetime
 from google.cloud import bigtable
+from google.cloud.bigtable import column_family
+from google.cloud.bigtable import row_filters
 
 # Service Layer -  business logic.
 # WHAT are we saving in the bigtable instance.
@@ -9,6 +12,20 @@ class BigtableClient:
 
         self.client = bigtable.Client(project='any-project', admin=True)
         self.instance = self.client.instance('any-instance')
+
+        print("Creating the {} table.".format(table_id))
+        table = instance.table(table_id)
+
+        print("Creating column family cf1 with Max Version GC rule...")
+        # Create a column family with GC policy : most recent N versions
+        # Define the GC policy to retain only the most recent 2 versions
+        max_versions_rule = column_family.MaxVersionsGCRule(2)
+        column_family_id = "cf1"
+        column_families = {column_family_id: max_versions_rule}
+        if not table.exists():
+            table.create(column_families=column_families)
+        else:
+            print("Table {} already exists.".format(table_id))
 
     def insert(self):
         pass
