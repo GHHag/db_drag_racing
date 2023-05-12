7062db58a5ceda25915d68fddac7e5257d3a5daa
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-05-12 12:06:24 +0200

    Gustav + Nikolai: Implemented Locust for Bigtable, delete is next.

--- locust-service/data/data.py
+++ locust-service/data/data.py
@@ -18,20 +18,21 @@ def format_json_data(json_file_path, hash_key, unique_key):
     with open(json_file_path, "r") as file:
         json_data = json.loads(file.read())
         for data_dict in json_data:
-            hash_ = data_dict.get(hash_key)
-            key = data_dict.get(unique_key)
-            data = {
-                k: v for k, v in data_dict.items() if k not in [hash_key, unique_key]
-            }
-            data_str = json.dumps(data)
-            formatted_data_list.append(
-                {hash_key: hash_, unique_key: key, "data": data_str}
-            )
-
-            # Example post request
-            # requests.post(
-            #    f"http://localhost:8080/redis/hset?hash={hash_key}&key={unique_key}&value={data_str}"
-            # )
+            # Selection for provided values of hash_key and unique_key
+            if hash_key or unique_key is not None:
+                hash_ = data_dict.get(hash_key)
+                key = data_dict.get(unique_key)
+                data = {
+                    k: v
+                    for k, v in data_dict.items()
+                    if k not in [hash_key, unique_key]
+                }
+                data_str = json.dumps(data)
+                formatted_data_list.append(
+                    {hash_key: hash_, unique_key: key, "data": data_str}
+                )
+            else:
+                formatted_data_list.append(data_dict)
 
     return formatted_data_list
 
@@ -39,14 +40,16 @@ def format_json_data(json_file_path, hash_key, unique_key):
 if __name__ == "__main__":
     # Call the format_json_data function and pass argument for the hashes and keys to be used
     # for querying the data
-    cars_data_list = format_json_data("cars_data.json", "id", "id")
-    reparations_data_list = format_json_data("reparations_data.json", "id", "id")
-    parts_data_list = format_json_data("parts_data.json", "id", "id")
+    # cars_data_list = format_json_data("cars_data.json", "id", "id")
+    # reparations_data_list = format_json_data("reparations_data.json", "id", "id")
+    # parts_data_list = format_json_data("parts_data.json", "id", "id")
+
+    cars_data_list = format_json_data("cars_data.json", None, None)
 
     pprint.pprint(cars_data_list[0])
     print()
-    pprint.pprint(reparations_data_list[0])
-    print()
-    pprint.pprint(parts_data_list[0])
-    print()
-    print(len(cars_data_list))
+    # pprint.pprint(reparations_data_list[0])
+    # print()
+    # pprint.pprint(parts_data_list[0])
+    # print()
+    # print(len(cars_data_list))
